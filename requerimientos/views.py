f3f069621cbf7d13b7dde22a064f10e957aabd28
Gabriel Iturra <gabrieliturrab@ug.uchile.cl>
2019-10-31 15:21:00 -0300

    se agregar primera versión formulario de requerimientos

--- requerimientos/views.py
+++ requerimientos/views.py
@@ -1,5 +1,12 @@
 from django.views.generic import TemplateView
+from .forms import ExampleForm
 
 
 class RequirementForm(TemplateView):
-    template_name = "requerimientos/add_postin.html"
+    template_name = "requerimientos/requerimientos.html"
+    http_method_names = ['get', 'post']
+
+    def get(self, request, *args, **kwargs):
+        if "form" not in kwargs:
+            kwargs['form'] = ExampleForm()
+            return super().get(request, *args, **kwargs)
