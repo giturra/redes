6d87b5bd157cd931bb5e78fa15275a1ebb38c301
Gabriel Iturra <gabrieliturrab@ug.uchile.cl>
2019-11-08 15:22:00 -0300

    Se agrega datos de usuarios son visibles desde el formulario de edición

--- perfiles/views.py
+++ perfiles/views.py
@@ -2,11 +2,14 @@ import os
 from .models import Perfil
 from django.urls import reverse_lazy
 from .forms import ProfileForm
+from .models import Perfil
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import View
 from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import UpdateView
+from django.views.generic import TemplateView
  
 
 # class ProfilesData(FormView):
@@ -29,28 +32,38 @@ from django.views.generic.edit import UpdateView
 #         response['Content-Disposition'] = 'inline;filename=cv.pdf'
 #         return response
 
-class Profile(View):
-    profile_form = ProfileForm()
-    context = {
-        'form':profile_form
-    }
-    template_name = 'perfiles/profile.html'
+# class Profile(View):
+#     profile_form = ProfileForm()
+#     context = {
+#         'form':profile_form
+#     }
+#     template_name = 'perfiles/profile.html'
 
-    def get(self, request, *args, **kwargs):
-        user = User.objects.get(username=request.user.username)
-        self.context['user'] = user
-        return render(request, self.template_name, self.context)
+#     def get(self, request, *args, **kwargs):
+#         user = User.objects.get(username=request.user.username)
+#         self.context['user'] = user
+#         return render(request, self.template_name, self.context)
     
-    def post(self, request, *args, **kwargs):
-        user = User.objects.get(username=request.user.username)
-        self.context['user'] = user
-        profile_form = ProfileForm(request.POST)
-        if profile_form.is_valid():
-            profile_form.save()
-        return render(request, self.template_name, context=self.context)
+#     def post(self, request, *args, **kwargs):
+#         user = User.objects.get(username=request.user.username)
+#         self.context['user'] = user
+#         profile_form = ProfileForm(request.POST)
+#         if profile_form.is_valid():
+#             profile_form.save()
+#         return render(request, self.template_name, context=self.context)
 # class Profile(UpdateView):
 #     model = Perfil
 #     success_url = reverse_lazy('/')
 #     fields = ['__all__']
 #     slug_field = 'username'
 #     slug_url_kwarg = 'username'
+
+class Profile(LoginRequiredMixin, TemplateView):
+    template_name = "perfiles/profile.html"
+    http_method_names = ['get', 'post']
+
+    def get(self, request, *args, **kwargs):
+        user = self.request.user
+        perfil = Perfil.objects.get(usuario=user)
+        kwargs['form'] = ProfileForm(instance=perfil)
+        return super(Profile, self).get(request, *args, **kwargs)
