0e1aa5d5eebb72db9ae270e4df1bbf6256faeae3
Gabriel Iturra <gabrieliturrab@ug.uchile.cl>
2019-11-08 13:44:32 -0300

    se agrega gitignore

--- redes/urls.py
+++ redes/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include(('app.urls', 'app'), namespace='app')),
+    path('api-auth/', include('rest_framework.urls')),
     path('requerimientos/', include(('requerimientos.urls', 'requerimientos'), namespace='requerimientos')),
     path('admin/', admin.site.urls),
 ]
