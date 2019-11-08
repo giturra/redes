e2b569897bef85a31fe276f2e578f6607fc704b5
Gabriel Iturra <gabrieliturrab@ug.uchile.cl>
2019-11-08 00:14:28 -0300

    se instala django rest y se crea app api para manejar la api para la app movil

--- redes/settings.py
+++ redes/settings.py
@@ -37,10 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
     'app',
     'crispy_forms',
     'perfiles',
-    'requerimientos'
+    'requerimientos',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
