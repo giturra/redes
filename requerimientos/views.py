3ad77acc5eeb576f54f680d1262f77b264676f61
Gabriel Iturra <gabrieliturrab@ug.uchile.cl>
2019-11-12 12:39:14 -0300

    se implementa que el form de carrera sea capaz de guardar la informacion de las carreras

--- requerimientos/views.py
+++ requerimientos/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import redirect
 from django.views.generic import FormView, ListView
 from django.forms import ValidationError
 from .forms import RequirementForm
@@ -8,7 +9,7 @@ from app.models import Carrera
 class Requirement(FormView):
     template_name = "requerimientos/requerimientos.html"
     form_class = RequirementForm
-    success_url = '/'
+    success_url = '/requerimientos/cupos'
 
     def form_valid(self, form):
         prod = form.cleaned_data['productora_nombre']
@@ -31,5 +32,12 @@ class QuotaRequirement(ListView):
     template_name = "requerimientos/requerimientos-cupos.html"
     model = Carrera
     context_object_name = "carreras"
+    http_method_names = ['get', 'post']
+
+    def post(self, request, *args, **kwargs):
+        for key, value in request.POST.items():
+            print(key, value)
+        return redirect('/requerimientos/cupos/')
+
     
     
