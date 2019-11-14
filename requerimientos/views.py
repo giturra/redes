4a698ce1231e2ba091269aae67141435a55c7ad3
Gabriel Iturra <gabrieliturrab@ug.uchile.cl>
2019-11-14 15:42:21 -0300

    hacer vista de exito al enviar el formulario

--- requerimientos/views.py
+++ requerimientos/views.py
@@ -3,7 +3,7 @@ from django.views.generic import FormView, ListView
 from django.forms import ValidationError
 from .forms import RequirementForm
 from .models import Cupos, Requerimiento, Producer
-from app.models import Carrera
+from app.models import Carrera, Empleo
 
 
 class Requirement(FormView):
@@ -67,43 +67,61 @@ def cupos_carreras(request):
             carrera_id = request.POST.get(nombre_carrera)
             if carrera_id is not None:
                 print(carrera_actual)
-                print(carrera)
-                # p1 = request.POST.get("prctica1_" + str(carrera_actual), 0)
-                # p2 = request.POST.get("prctica2_" + str(carrera_actual), 0)
-                # p3 = request.POST.get("prctica3_" + str(carrera_actual), 0)
-                # memoria = request.POST.get("memoria_" + str(carrera_actual), 0)
-                # ft = request.POST.get("fulltime_" + str(carrera_actual), 0)
-                # pt = request.POST.get(u'_'.join(("parttime", carrera_actual)), 0)
+                #print(carrera)
+                p1 = request.POST.get("prctica1_" + str(carrera_actual), 0)
+                p2 = request.POST.get("prctica2_" + str(carrera_actual), 0)
+                p3 = request.POST.get("prctica3_" + str(carrera_actual), 0)
+                me = request.POST.get("memoria_" + str(carrera_actual), 0)
+                pt = request.POST.get("parttime_" + str(carrera_actual), 0)
+                ft = request.POST.get("fulltime_" + str(carrera_actual), 0)
+                #pt = request.POST.get(u'_'.join(("parttime", carrera_actual)), 0)
 
                 carrera = Carrera.objects.get(id=carrera_id)
-                
+                print(p1)
                 requirement_id = request.COOKIES.get('req_id')
                 #print(requirement_id)
                 requirement = Requerimiento.objects.get(id=requirement_id)
 
-                # if p1 and p1 != 0:
-                #     p1_data = Cupos(companie_requirement=requirement, degree=degree, tipo='P1', cupos=p1)
-                #     p1_data.save()
-                # if p2 and p2 != 0:
-                #     p2_data = Cupos(companie_requirement=requirement, degree=degree, tipo='P2', cupos=p2)
-                #     p2_data.save()
-                # if p3 and p3 != 0:
-                #     p3_data =Cupos(companie_requirement=requirement, degree=degree, tipo='P3', cupos=p3)
-                #     p3_data.save()
-                # if memoria and memoria != 0:
-                #     m_data = CuposCarrera(companie_requirement=requirement, degree=degree, tipo='ME',
-                #                                  cupos=memoria)
-                #     m_data.save()
-                # if ft and ft != 0:
-                #     tr_data = Cupos(companie_requirement=requirement, degree=degree, tipo='FT', cupos=ft)
-                #     tr_data.save()
-                # if pt and pt != 0:
-                #     pt_data = Cupos(companie_requirement=requirement, degree=degree, tipo='PT', cupos=pt)
-                #     pt_data.save()
+                if p1 and p1 != 0:
+                    plan = Empleo.objects.get(id=1)
+                    p1_data = Cupos(cupos=p1, requerimiento=requirement)
+                    p1_data.save()
+                    p1_data.carrera.add(carrera)
+                    p1_data.plan.add(plan)
+                if p2 and p2 != 0:
+                    plan = Empleo.objects.get(id=2)
+                    p2_data = Cupos(cupos=p2, requerimiento=requirement)
+                    p2_data.save()
+                    p2_data.carrera.add(carrera)
+                    p2_data.plan.add(plan)
+                if p3 and p3 != 0:
+                    plan = Empleo.objects.get(id=3)
+                    p3_data = Cupos(cupos=p3, requerimiento=requirement)
+                    p3_data.save()
+                    p3_data.carrera.add(carrera)
+                    p3_data.plan.add(plan)
+                if me and me != 0:
+                    plan = Empleo.objects.get(id=4)
+                    me_data = Cupos(cupos=me, requerimiento=requirement)
+                    me_data.save()
+                    me_data.carrera.add(carrera)
+                    me_data.plan.add(plan)
+                if pt and pt != 0:
+                    plan = Empleo.objects.get(id=5)
+                    pt_data = Cupos(cupos=pt, requerimiento=requirement)
+                    pt_data.save()
+                    pt_data.carrera.add(carrera)
+                    pt_data.plan.add(plan)
+                if ft and ft != 0:
+                    plan = Empleo.objects.get(id=6)
+                    ft_data = Cupos(cupos=pt, requerimiento=requirement)
+                    ft_data.save()
+                    ft_data.carrera.add(carrera)
+                    ft_data.plan.add(plan)
 
                 carreras_restantes -= 1
             #carrera_actual += 1
-        return render(request, 'requirements/exito.html')
+        return render(request, 'requerimientos/requerimientos-exito.html')
     else:
         print("wenaanannana")
         if not 'cupos' in request.session:
